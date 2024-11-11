1f238d6b4829c47a1fcdfd4fb49683fb307ec27d
EzequielBoucher <ezequielboucher34@gmail.com>
2024-11-11 13:48:47 -0300

    atividade 11

---
+++ logica.js
@@ -0,0 +1,182 @@
+// ******************************************** Primeiro tópico
+
+//  const numero = parseInt(prompt('Escolha um numero entre 1 e 3'))
+//  switch(numero){
+//      case 1:
+//          console.log('voce escolheu o numero um')
+//          break
+//      case 2:
+//          console.log('voce escolheu o numero dois')
+//          break
+//      case 3:
+//         console.log('voce escolheu o numero tres')
+//         break
+
+//      default:
+//          console.log('numero invalido')
+//  }
+
+// ******************************************** Segundo tópico
+
+// const comparacao = 0
+// let maior = 10
+
+// let numero = parseInt(prompt('Escolha um numero numero'))
+
+//     if(numero > maior){
+//         maior = numero
+
+//         console.log('esse numero é maior que 10')
+//     }else {
+//         console.log('esse numero é menor que 10')
+//     }
+
+// ******************************************** Terceiro tópico
+
+// let dia = parseInt(prompt("Digite um número de 1 a 7 para o dia da semana:"));
+
+// switch (dia) {
+//     case 1:
+//         alert("Domingo");
+//         break;
+//     case 2:
+//         alert("Segunda-feira");
+//         break;
+//     case 3:
+//         alert("Terça-feira");
+//         break;
+//     case 4:
+//         alert("Quarta-feira");
+//         break;
+//     case 5:
+//         alert("Quinta-feira");
+//         break;
+//     case 6:
+//         alert("Sexta-feira");
+//         break;
+//     case 7:
+//         alert("Sábado");
+//         break;
+//     default:
+//         alert("Dia inválido!");
+// }
+
+// ******************************************** Quarto tópico
+
+// let cor = prompt("Digite uma cor (vermelho, verde, azul):").toLowerCase();
+
+// switch (cor) {
+//     case "vermelho":
+//         alert("Você escolheu a cor vermelha.");
+//         break;
+//     case "verde":
+//         alert("Você escolheu a cor verde.");
+//         break;
+//     case "azul":
+//         alert("Você escolheu a cor azul.");
+//         break;
+//     default:
+//         alert("Cor inválida!");
+// }
+
+// ******************************************** Quinto tópico
+
+// let numero1 = parseInt(prompt("Digite o primeiro número:"));
+// let numero2 = parseInt(prompt("Digite o segundo número:"));
+
+// if (numero1 % 2 === 0 && numero2 % 2 === 0) {
+//     alert("Ambos os números são pares.");
+// } else {
+//     alert("Pelo menos um dos números não é par.");
+// }
+
+// ******************************************** sexto tópico
+
+// let operacao = prompt("Digite uma operação (+, -, *, /):");
+// let num1 = parseFloat(prompt("Digite o primeiro número:"));
+// let num2 = parseFloat(prompt("Digite o segundo número:"));
+
+// let resultado;
+
+// switch (operacao) {
+//     case "+":
+//         resultado = num1 + num2;
+//         break;
+//     case "-":
+//         resultado = num1 - num2;
+//         break;
+//     case "*":
+//         resultado = num1 * num2;
+//         break;
+//     case "/":
+//         if (num2 !== 0) {
+//             resultado = num1 / num2;
+//         } else {
+//             alert("Não é possível dividir por zero!");
+//             break;
+//         }
+//         break;
+//     default:
+//         alert("Operação inválida!");
+//         break;
+// }
+
+// if (resultado !== undefined) {
+//     alert("Resultado: " + resultado);
+// }
+
+// ******************************************** setimo tópico
+
+// let nota = parseFloat(prompt("Digite uma nota de 0 a 10:"));
+
+// if (nota <= 4.9) {
+//     alert("Nota Baixa");
+// } else if (nota >= 5 && nota <= 6.9) {
+//     alert("Nota Média");
+// } else if (nota >= 7) {
+//     alert("Nota Alta");
+// } else {
+//     alert("Nota inválida!");
+// }
+
+// ******************************************** oitavo tópico
+
+// let estadoCivil = prompt("Digite seu estado civil (solteiro, casado, divorciado, viúvo):").toLowerCase();
+
+// switch (estadoCivil) {
+//     case "solteiro":
+//         alert("Você é solteiro.");
+//         break;
+//     case "casado":
+//         alert("Você é casado.");
+//         break;
+//     case "divorciado":
+//         alert("Você é divorciado.");
+//         break;
+//     case "viúvo":
+//         alert("Você é viúvo.");
+//         break;
+//     default:
+//         alert("Estado civil inválido!");
+// }
+
+// ******************************************** nono tópico
+
+// let numero = parseInt(prompt("Digite um número:"));
+
+// if (numero % 2 === 0) {
+//     alert("O número é par.");
+// } else {
+//     alert("O número é ímpar.");
+// }
+
+// ******************************************** decimo tópico
+
+// let idade = parseInt(prompt("Digite sua idade:"));
+
+// if (idade >= 18) {
+//     alert("Você é maior de idade.");
+// } else {
+//     alert("Você é menor de idade.");
+// }
+
